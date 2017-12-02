5232e940a25682b1c61d016e1e0ae3a539719e73
kosmo5-12 <kwlsy666@naver.com>
2017-12-02 17:09:47 +0900

    자료형

--- 2.py
+++ 2.py
@@ -1,2 +1,27 @@
 a = [1,2,3]
 print(a)
+
+a = [1,2,3,4,5]
+print(a[0:2])
+print(a[2:5])
+
+b = a[:2]
+c = a[2:]
+print(b)
+print(c)
+
+#stack LIFO
+#Queue
+#리스트 []
+#튜플 ()
+#리스트와 튜플의 차이점
+
+#딕셔너리-사전
+#{}사용, 연관배열 또는 해시라고 한다.
+# Key, Value
+a = {1:'a'}
+a[2] = 'b'
+print(a)
+
+#리스트/튜플/딕셔너리 - DB
+#int float bool None -기본 자료형
