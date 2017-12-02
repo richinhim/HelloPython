946f4220e37e0846fef01ae4b2c1a4a583a10f65
kosmo5-12 <kwlsy666@naver.com>
2017-12-02 18:03:50 +0900

    리스트 튜플 딕셔너리

---
+++ 4.py
@@ -0,0 +1,27 @@
+naver_end_price = [488500,501500,461500,510000]
+
+print(naver_end_price)
+
+max_price = max(naver_end_price)
+
+print(max(naver_end_price))
+
+min_price = min(naver_end_price)
+print(min(naver_end_price))
+
+print(max(naver_end_price) - min(naver_end_price))
+
+print("가격차: ", max_price - min_price)
+
+print(naver_end_price[2]);
+
+naver_end_price1 = {'월요일': 488500,'화요일':501500,'수요일':461500,'목요일':510000}
+
+print(naver_end_price1['수요일'])
+
+
+naver_end_price2 = {'9/11': 488500,'9/10':501500,'9/12':461500,'9/9':510000}
+
+
+print(naver_end_price2['9/9'])
+
